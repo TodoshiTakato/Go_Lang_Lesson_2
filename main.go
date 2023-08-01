c5ac7b03664bcd35f19f83480312aed1c12d2ef4
Нурлан Сарсенбаев <nsarsenbaev@paymart.uz>
2023-08-01 19:04:00 +0500

    DEV-0001/feature branch: ne uspel zakonchit

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/exp/slices"
+)
 
 const (
 	exit = "exit"
@@ -10,38 +13,71 @@ const (
 
 func main() {
 	var command string
-	userList := []string{"user1_password1", "user1_password1"}
-	productList := make([]string, 0, 10)
+	userList := []string{"user1_password1", "user2_password2"}
 
-	_ = productList
+	//productList := make([]string, 0, 10) // Сделать список продуктов
+	productList := map[string]int{
+		"Молоко":         100,
+		"Кефир":          100,
+		"Йогурт":         100,
+		"Кашка-Экспресс": 100,
+		"Хлеб":           100,
+		"Помидоры":       100,
+		"Огурцы":         100,
+		"Морковь":        100,
+		"Мясо говяжее":   100,
+		"Банан":          100}
+
 	for command != exit {
-		fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
-		fmt.Scan(&command)
+		//fmt.Println("Введите команду") // Сделать красивый вывод, вывести список команд на этом шаге
+		fmt.Printf("Введите команду\nСписок команд:\n\t%q\n\t%q\n\t%q", reg, auth, exit)
+
+		_, err := fmt.Scan(&command)
+		if err != nil {
+			return
+		}
 
 		switch command {
 		case exit:
 			break
 		case reg:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command) // Сделать так, что бы выводил сообщение, если пользователь уже существует
-			userList = append(userList, command)
+			fmt.Println("Для регистрации введите логин и пароль в формате login_password")
 
-			message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
-			fmt.Println(message)
+			_, err := fmt.Scan(&command)
+			if err != nil {
+				return
+			}
 
-			fmt.Println(userList)
+			if slices.Contains(userList, command) { // Сделать так, что бы выводил сообщение, если пользователь уже существует
+				fmt.Println("Такой пользователь уже существует!")
+				fmt.Println(userList)
+			} else {
+				userList = append(userList, command)
+				message := fmt.Sprintf("Пользователь %s успешно добавлен", command)
+				fmt.Println(message)
+				fmt.Println(userList)
+			}
 		case auth:
-			fmt.Println("Введите логин и пароль в таком виде login_passwor")
-			fmt.Scan(&command)
+			fmt.Println("Для авторизации введите логин и пароль в формате login_password")
+
+			_, err := fmt.Scan(&command)
+			if err != nil {
+				return
+			}
 
 			for _, v := range userList {
 				if v == command {
-					fmt.Println("Добро пожаловать в магази")
+					fmt.Println("Добро пожаловать в магазин!")
+
+					fmt.Println("Выберите интересующий вас продукт:")
+
+					for product, count := range productList {
+						fmt.Printf("%q: %d", product, count)
+					}
 
 				} else {
-					fmt.Println("Вы не зарегистрированны")
+					fmt.Println("Вы не зарегистрированы!")
 				}
-
 			}
 		}
 	}
